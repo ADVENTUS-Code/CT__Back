56bf3515bb6594cc4b61d28ebf490f2a42428aed
skataxe2 <axel.garcia9613@gmail.com>
2022-10-03 11:04:17 +0200

    no os secret key

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ import requests
 import matplotlib
 import json
 from apscheduler.schedulers.background import BackgroundScheduler
-import os
 
 
 matplotlib.use('Agg')
@@ -35,7 +34,7 @@ app.config['SESSION_USE_SIGNER'] = True
 # port = os.environ.get("REDIS_PORT")
 # app.config['SESSION_REDIS'] = redis.from_url(
 #     "redis://:p039990fa3f0e147607f7c1b997d8212cdfd930d4d836c44f6d21598f8d62a7fe@ec2-18-202-190-160.eu-west-1.compute.amazonaws.com:22730")
-app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SECRET_KEY'] = 'put_my_secret_key_here'
 app.config['SESSION_COOKIE_NAME'] = "my_session"
 
 
