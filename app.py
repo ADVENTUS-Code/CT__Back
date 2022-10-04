2d36623a82b142a5090858af9e5d49094e535431
skataxe2 <axel.garcia9613@gmail.com>
2022-10-04 10:31:12 +0200

    install npm redis

--- app.py
+++ app.py
@@ -36,12 +36,9 @@ app.config['SESSION_USE_SIGNER'] = True
 # app.config['SESSION_REDIS'] = Redis(
 #     host="ec2-52-215-145-250.eu-west-1.compute.amazonaws.com", port=10430)
 # port = os.environ.get("REDIS_PORT")
-# app.config['SESSION_REDIS'] = redis.from_url(
-#     "redis://:pd037a52a3f1f0820f749ba365534ddf5751bbc706b411b7996468c8b9eff866f@ec2-52-215-145-250.eu-west-1.compute.amazonaws.com:10430")
-url = urlparse(os.environ.get("REDIS_URL"))
-r = redis.Redis(host=url.hostname, port=url.port, username=url.username,
-                password=url.password, ssl=True, ssl_cert_reqs=None)
-# app.config['REDIS_URL'] = 'redis://:pd037a52a3f1f0820f749ba365534ddf5751bbc706b411b7996468c8b9eff866f@ec2-34-241-122-201.eu-west-1.compute.amazonaws.com:20849'
+app.config['SESSION_REDIS'] = redis.from_url(
+    "redis://:pd037a52a3f1f0820f749ba365534ddf5751bbc706b411b7996468c8b9eff866f@ec2-34-241-122-201.eu-west-1.compute.amazonaws.com:20849")
+
 app.config['SECRET_KEY'] = 'put_my_secret_key_here'
 app.config['SESSION_COOKIE_NAME'] = "my_session"
 
