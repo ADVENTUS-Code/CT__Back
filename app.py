4bbc71b29404a313b743060ce1594adcd83da356
skataxe2 <axel.garcia9613@gmail.com>
2022-10-10 12:03:25 +0200

    session permanant

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ port = os.environ.get("REDIS_PORT")
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ffbtctbuykfwze:f8f3ce9ef5eb1c755a1606cf333d51bd83ce373c836b208376aada71325d0c95@ec2-34-251-115-141.eu-west-1.compute.amazonaws.com:5432/dd7ha3irla8lna"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_TYPE'] = "filesystem"
-app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_PERMANENT'] = True
 app.config['SESSION_USE_SIGNER'] = True
 # port = os.environ.get("REDIS_PORT")
 # app.config['SESSION_REDIS'] = redis.from_url(
@@ -212,7 +212,6 @@ class Users(db.Model):
 
 
 # ---------------------------------Flask authentification --------------------------------------------------
-
 
 
 @ cross_origin
